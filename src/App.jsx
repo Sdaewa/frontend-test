0cec9d475d7548452d573305aa2f1d3c2aeae8df
Luis Ramirez <sdaewa22@gmail.com>
2021-12-04 11:02:30 +0100

    calendar slots

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Typography } from '@mui/material';
+// import { useTranslation } from 'react-i18next';
+import {
+  Typography,
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default () => {
-  const { t } = useTranslation();
-  return <Typography>{t('happyHolidays')}</Typography>;
+// export default () => {
+//   const { t } = useTranslation();
+//   return <Typography>{t('happyHolidays')}</Typography>;
+// };
+
+const slots = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+  28, 29, 30, 31,
+];
+
+const theme = createTheme();
+
+const Calendar = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Calendar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            {slots.map((slot) => (
+              <Grid item key={slot} xs={12} sm={6} md={4}>
+                <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Day X
+                    </Typography>
+                    <Typography>Some text</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Open</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
 };
+
+export default Calendar;
